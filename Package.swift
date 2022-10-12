de3ec100865740a79e70a7eca8d6af89420b4319
Gitlab CI Network <network@sportradar.com>
2022-10-12 09:00:28 +0200

    WidgetSDK 0.4.7 release

---
+++ Package.swift
@@ -0,0 +1,41 @@
+
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "WidgetSDK",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "WidgetSDK",
+            targets: ["WidgetSDKTarget"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "git@github.com:sportradar/mobile-us-widgets-shared.git", exact: "0.4.7"),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.6.0"),
+        .package(url: "https://github.com/amplitude/Amplitude-iOS", from: "8.10.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WidgetSDK",
+            url: "https://artifacts.mdp.sportradar.com/apple/native-widgets-core/6a5310ab/release/WidgetSDK-iOS-0.4.7.zip",
+            checksum: "e7c0ffba201bc39659b29f0041fd160b4b5fe5423ca38441b44da82ba1270f6c"
+        ),
+        .target(
+            name: "WidgetSDKTarget",
+                dependencies: [
+                    .target(name: "WidgetSDK"),
+                    .product(name: "shared", package: "native-widgets-spm-shared"),
+                    .product(name: "Alamofire", package: "Alamofire"),
+                    .product(name: "SnapKit", package: "SnapKit"),
+                    .product(name: "Amplitude", package: "Amplitude-iOS")
+                ],
+            path: "WidgetSDKTarget")
+    ]
+)
+
+
