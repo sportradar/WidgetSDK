e5e4d1f2a848770d85c857daf4428df96f293efe
Gitlab CI Network <network@sportradar.com>
2023-08-18 13:53:57 +0200

    WidgetSDK 0.4.13 release

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/sportradar/mobile-us-widgets-shared", exact: "0.4.12"),
+        .package(url: "https://github.com/sportradar/mobile-us-widgets-shared", exact: "0.4.13"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.0"),
         .package(url: "https://github.com/SnapKit/SnapKit", from: "5.6.0"),
         .package(url: "https://github.com/amplitude/Amplitude-iOS", from: "8.10.0")
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WidgetSDK",
-            url: "https://artifacts.mdp.sportradar.com/apple/native-widgets-core/0d5baaa2/release/WidgetSDK-iOS-0.4.12.zip",
-            checksum: "cc122a64799d27e5fb9bcc0992aa19cd7a9d8683092dd5934485b0d78a54174d"
+            url: "https://artifacts.mdp.sportradar.com/apple/native-widgets-core/ba642cee/release/WidgetSDK-iOS-0.4.13.zip",
+            checksum: "0f880e52d837eae13da77df07204e94622a1634446630e59dae2c402562b5d95"
         ),
         .target(
             name: "WidgetSDKTarget",
